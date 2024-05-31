13834abded876fe7baf60f1751e6c527a298a24f
afmika <afmichael73@gmail.com>
2024-05-31 18:57:21 +0300

    test: emulate events

--- tests/complex/test.py
+++ tests/complex/test.py
@@ -4,20 +4,40 @@ import asyncio
 
 import pytest
 
-from tests.complex.utils import LogFilter, WorkflowTest
+from tests.complex.utils import LogFilter, StepError, TimeStep, WorkflowTest
+from tests.complex.workflows.events import event_workflow
 from tests.complex.workflows.simple import simple_workflow
 
 @pytest.mark.asyncio(scope="class") # ensure one main loop per class
 class TestInOneEventLoopPerClass(WorkflowTest):
+    async def test_async(t):
+        await asyncio.sleep(1)
+        assert 1 + 1 is 2
+
     async def test_test(t):
-        await asyncio.sleep(0.2)
-        assert True
+        with pytest.raises(StepError) as info:
+            t.step("A").logs_data_equal(LogFilter.runs, [])
+        assert info.value.args[0] == "'A': No workflow has been run prior the call"
 
     async def test_simple(t):
-        s = t.step().timeout(3)
-        s = await s.exec_workflow(simple_workflow, "simple", 1)
+        s = await (
+            t
+            .step()
+            .timeout(3)
+            .exec_workflow(simple_workflow)
+        )
         (
             s
             .logs_data_equal(LogFilter.runs, ['A', 'B A', 'C B A'])
             .logs_data_equal(LogFilter.event, [])
         )
+
+    async def test_events(t):
+        s = t.step().timeout(10)
+        s = await (
+            s
+            .timeline(TimeStep("sayHello", 1, "Hello from outside!"))
+            .timeline(TimeStep("cancel", 5))
+            .exec_workflow(event_workflow)
+        )
+        s.logs_data_equal(LogFilter.runs, ['A', 'Hello from outside! B A'])
--- tests/complex/utils.py
+++ tests/complex/utils.py
@@ -1,35 +1,49 @@
 # Equiv. server + worker
 import asyncio
+from dataclasses import asdict, dataclass
 from enum import Enum
-from typing import List
+from typing import List, Union
 
 from substantial.conductor import Recorder, SubstantialMemoryConductor
 from substantial.types import Log
+from substantial.workflow import Workflow, WorkflowRun
 
 class LogFilter(str, Enum):
     event = "event"
     runs = "runs"
 
+@dataclass
+class TimeStep:
+    event_name: str
+    delta_time: int
+    payload: Union[any, None] = None
+
 class StepError(Exception):
     def __init__(self, step: str, message) -> None:
-        super().__init__(f"At step '{step}': {message}")
+        super().__init__(f"'{step}': {message}")
 
 class WorkflowTest:
     timeout_secs = 120
     name: str
-    curr_handle: str | None = None
-    curr_record: Recorder | None = None
+    handle: str | None = None
+    recorder: Recorder | None = None
+    event_timeline: List[TimeStep] = []
 
     def error(self, message: str):
         return StepError(self.name, message)
 
     def step(self, name: str | None = None):
+        """ Prepare a new test scope """
         runner = WorkflowTest()
         runner.name = name or "<unnamed>"
         return runner
 
     def timeout(self, timeout_secs: int):
         self.timeout_secs = timeout_secs
+        return self
+    
+    def timeline(self, time_step: TimeStep):
+        self.event_timeline.append(time_step)
         return self
 
     def logs_data_equal(
@@ -38,13 +52,13 @@ class WorkflowTest:
         other: List[Log] | List[any],
     ):
         res = []
-        if self.curr_record is None or self.curr_handle is None:
+        if self.recorder is None or self.handle is None:
             raise self.error("No workflow has been run prior the call")
         
         if filter.name == LogFilter.runs:
-            res = self.curr_record.get_recorded_runs(self.curr_handle)
+            res = self.recorder.get_recorded_runs(self.handle)
         else:
-            res = self.curr_record.get_recorded_events(self.curr_handle)
+            res = self.recorder.get_recorded_events(self.handle)
         if len(res) == 0 and len(other) == 0:
             return self
         if isinstance(res[0], Log):
@@ -61,32 +75,40 @@ class WorkflowTest:
         delta_time_secs: List[int] | int
     ):
         """ Compare elapsed time in between two rows """
-        pass
+        # idea: run a workflow multiple times, keep the recorded runs
+        # then compare
+        raise Exception("TODO")
 
-    async def exec_workflow(
-        self,
-        workflow,
-        name: str,
-        version: int,
-    ):
+    async def exec_workflow(self, workflow: Workflow):
         substantial = SubstantialMemoryConductor()
         substantial.register(workflow)
         backend_exec = asyncio.create_task(substantial.run())
 
-        workflow_run = workflow(name, version)
+        workflow_run = workflow(0, "<default>")
         handle = workflow_run.handle
 
         async def go():
-            await substantial.start(workflow_run),
+            await substantial.start(workflow_run)
+            for ev in self.event_timeline:
+                await asyncio.sleep(ev.delta_time)
+                await substantial.send(handle, ev.event_name, ev.payload)
             await backend_exec
+
         try:
             await asyncio.wait_for(go(), self.timeout_secs)
         except TimeoutError:
             pass
         except:
             raise
-        
-        self.curr_record = substantial.runs
-        self.curr_handle = handle 
+
+        self.recorder = substantial.runs
+        self.handle = handle 
 
         return self
+
+
+    # async def replay(self, count: int):
+    #     pass
+    # Not sure about the feasability since 
+    # replay is hidden inside the backend's workflow queue and is is not reachable from outside
+    # though it can be simulated with events
