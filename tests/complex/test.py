5e7b0966283d128b4d289386ad0fdaad9b1dc0bf
afmika <afmichael73@gmail.com>
2024-06-03 16:30:22 +0300

    experiment: one main loop per module

--- tests/complex/test.py
+++ tests/complex/test.py
@@ -1,71 +1,77 @@
 
 
 import asyncio
-from dataclasses import dataclass
-
 import pytest
-
+from dataclasses import dataclass
 from tests.complex.utils import LogFilter, StepError, TimeStep, WorkflowTest
 
 from substantial.workflow import workflow, Context
 
-@pytest.mark.asyncio(scope="function") # ensure one main loop per function
-class TestInOneEventLoopPerClass(WorkflowTest):
-    async def test_async(t):
-        await asyncio.sleep(1)
-        assert 1 + 1 is 2
+# Further configuration details:
+# https://pytest-asyncio.readthedocs.io/en/latest/how-to-guides/multiple_loops.html
 
-    async def test_test(t):
-        with pytest.raises(StepError) as info:
-            t.step("A").logs_data_equal(LogFilter.runs, [])
-        assert info.value.args[0] == "'A': No workflow has been run prior the call"
+@pytest.mark.asyncio(scope="module")
+async def test_async():
+    await asyncio.sleep(1)
+    assert 1 + 1 is 2
 
-    async def test_simple(t):
-        @workflow("simple")
-        async def simple_workflow(c: Context, name, n):
-            r1 = await c.save(lambda: "A")
-            r2 = await c.save(lambda: f"B {r1}")
-            r3 = await c.save(lambda: f"C {r2}")
-            return r3
+@pytest.mark.asyncio(scope="module")
+async def test_test():
+    t = WorkflowTest()
+    with pytest.raises(StepError) as info:
+        t.step("A").logs_data_equal(LogFilter.runs, [])
+    assert info.value.args[0] == "'A': No workflow has been run prior the call"
 
-        s = await (
-            t
-            .step()
-            .timeout(3)
-            .exec_workflow(simple_workflow)
-        )
-        (
-            s
-            .logs_data_equal(LogFilter.runs, ['A', 'B A', 'C B A'])
-            .logs_data_equal(LogFilter.event, [])
-        )
+@pytest.mark.asyncio(scope="module")
+async def test_simple():
+    @workflow("simple")
+    async def simple_workflow(c: Context, name, n):
+        r1 = await c.save(lambda: "A")
+        r2 = await c.save(lambda: f"B {r1}")
+        r3 = await c.save(lambda: f"C {r2}")
+        return r3
 
-    async def test_events(t):
-        @dataclass
-        class State:
-            is_cancelled: bool
-            def update(self, *_):
-                self.is_cancelled = True
+    t = WorkflowTest()
+    s = await (
+        t
+        .step()
+        .timeout(3)
+        .exec_workflow(simple_workflow)
+    )
+    (
+        s
+        .logs_data_equal(LogFilter.runs, ['A', 'B A', 'C B A'])
+        .logs_data_equal(LogFilter.event, [])
+    )
 
-        @workflow(1, "events")
-        async def event_workflow(c: Context, name, n):
-            r1 = await c.save(lambda: "A")
-            payload = await c.event("sayHello")
+@pytest.mark.asyncio(scope="module")
+async def test_events():
+    @dataclass
+    class State:
+        is_cancelled: bool
+        def update(self, *_):
+            self.is_cancelled = True
 
-            s = State(False)
-            c.register("cancel", s.update)
-            await c.wait(lambda: s.is_cancelled)
+    @workflow(1, "events")
+    async def event_workflow(c: Context, name, n):
+        r1 = await c.save(lambda: "A")
+        payload = await c.event("sayHello")
 
-            if s.is_cancelled:
-                r3 = await c.save(lambda: f"{payload} B {r1}")
-            return r3
+        s = State(False)
+        c.register("cancel", s.update)
+        await c.wait(lambda: s.is_cancelled)
 
-        s = t.step().timeout(10)
-        s = await (
-            s
-            .timeline(TimeStep("sayHello", 1, "Hello from outside!"))
-            .timeline(TimeStep("cancel", 5))
-            .exec_workflow(event_workflow)
-        )
-        s.logs_data_equal(LogFilter.runs, ['A', 'Hello from outside! B A'])
+        if s.is_cancelled:
+            r3 = await c.save(lambda: f"{payload} B {r1}")
+        return r3
+
+    t = WorkflowTest()
+    s = t.step().timeout(10)
+    s = await (
+        s
+        .timeline(TimeStep("sayHello", 1, "Hello from outside!"))
+        .timeline(TimeStep("cancel", 5))
+        .exec_workflow(event_workflow)
+    )
+    s.logs_data_equal(LogFilter.runs, ['A', 'Hello from outside! B A'])
 
