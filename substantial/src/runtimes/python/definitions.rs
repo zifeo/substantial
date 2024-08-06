285d017bfb362570bd00a448bed3eaf416a83cb8
afmika <afmichael73@gmail.com>
2024-08-06 18:59:43 +0300

    wip: async

--- substantial/src/runtimes/python/definitions.rs
+++ substantial/src/runtimes/python/definitions.rs
@@ -1,28 +1,35 @@
 use pyo3::prelude::*;
 
-#[pyfunction]
-fn double(x: usize) -> usize {
-    x * 2
+// submodules issues
+// https://github.com/PyO3/pyo3/discussions/3591
+// https://github.com/PyO3/pyo3/issues/759
+
+// better async
+// https://github.com/PyO3/pyo3/issues/1632
+
+#[pymodule]
+pub fn substantial(m: Bound<PyModule>) -> PyResult<()> {
+    m.add_class::<Context>()?;
+    Ok(())
 }
 
-#[pymodule(name = "substantial")]
-fn substantial(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(double, m)?)?;
-    m.add("Context", Context)
-}
-
-// Define the Context struct
 #[pyclass]
 pub struct Context;
 
 #[pymethods]
 impl Context {
-    #[new]
-    fn new() -> Self {
-        Context
-    }
-
-    fn save(&self, value: i32) -> PyResult<i32> {
-        Ok(value + 5)
+    fn save(&self, lambda: Bound<PyAny>) -> PyResult<i32> /*PyResult<&'_ Bound<'_, PyAny>>*/ {
+        // save(lambda: coro(..) | value)
+        if !lambda.is_callable() {
+            return Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(format!(
+                "{:?} is not callable",
+                lambda
+            )));
+        }
+        // FIXME: wrong lifetime, make global? => who should own it?
+        // let coro_or_val = lambda.call0()?.as_ref();
+        // Ok(coro_or_val)
+        // ALT: write Context in python but each method will call a rust function
+        Ok(1234)
     }
 }
--- substantial/src/runtimes/python/mod.rs
+++ substantial/src/runtimes/python/mod.rs
@@ -1,6 +1,7 @@
 use std::path::{Path, PathBuf};
 
 use anyhow::{Context, Ok};
+use definitions::substantial;
 use pyo3::prelude::*;
 use serde_json::Value;
 use serde_pyobject::from_pyobject;
@@ -35,17 +36,29 @@ impl super::WorkflowRunner for PythonRunner {
                 .with_context(|| format!("Get parent of {}", workflow_file.display()))?,
         );
 
+        pyo3::append_to_inittab!(substantial); // requires auto-initialize off
         pyo3::prepare_freethreaded_python();
+
         Python::with_gil(|py| {
             debug_sys!(py);
-            // let substantial = PyModule::import_bound(py, "substantial")?;
 
+            // setup loop
+            let asyncio = py.import_bound("asyncio")?;
+            let policy = asyncio.getattr("get_event_loop_policy")?.call0()?;
+            let ev_loop = policy.call_method0("new_event_loop")?;
+            asyncio.call_method1("set_event_loop", (ev_loop.clone(),))?;
+
+            // setup module
             let wf_entry_point = workflow_file.file_stem().unwrap().to_str().unwrap();
             let wf_mod = py.import_bound(wf_entry_point)?;
 
+            // call workflow
             let context = definitions::Context;
-            let output = wf_mod.call_method(name, (context,), None)?;
+            let coro = wf_mod.call_method1(name, (context,))?;
+            let output = ev_loop.call_method1("run_until_complete", (coro,))?;
+
             from_pyobject(output).map_err(|e| e.into())
+            // serde_json::to_value(&output.to_string()).map_err(|e| e.into())
         })
     }
 
